6081fe137a3dd05e5725306e06eff018d49bf0cf
pytran3 <34180469+pytran3@users.noreply.github.com>
2020-05-04 17:22:49 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
-import Hello from "./Hello";
+import TodoInput from "./TodoInput";
+import TodoList from "./TodoList";
 
-function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">Welcome to React</h1>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-              <ul>
-                {["foo", "bar"].map(name => <li><Hello name={name}/></li>)}
-                  <li><Hello name={"hoge"}/></li>
-                  <li><Hello /></li>
-                <li><Hello/></li>
-              </ul>
-            </header>
-        </div>
-    );
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            tasks: [
+                {
+                    title: 'デフォルトTODO',
+                    id: 0
+                }
+            ],
+            uniqueId: 1
+        };
+    }
+
+    addTodo = title => {
+        const {
+            tasks,
+            uniqueId
+        } = this.state;
+
+        const task = {
+            title,
+            uniqueId
+        };
+
+        const newTasks = [...tasks, task];
+
+        this.setState({
+            tasks: newTasks,
+            uniqueId: uniqueId + 1
+        });
+    };
+
+    resetTodo = () => {
+        this.setState({
+            tasks: []
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>TODO App</h1>
+                <button onClick={this.resetTodo}>リセット</button>
+                <TodoInput addTodo={this.addTodo}/>
+                <TodoList tasks={this.state.tasks}/>
+            </div>
+        )
+    }
 }
-
-export default App;
